51e38fff4e5a0e509b6fbb84bdfe273bcf08ee1b
cking616 <cking616@mail.ustc.edu.cn>
2017-05-11 11:49:15 +0800

    完善对原始数据的分析

--- __main__.py
+++ __main__.py
@@ -14,8 +14,27 @@ from collections import defaultdict
 import xlwt
 
 
-def analysis_original_file():
-    pass
+def analysis_original_file(original_filename, raw_filename, target_year, target_month):
+    original_file = open(original_filename)
+
+    # current_year = 2000
+    # current_month = 0
+    raw_dict = defaultdict(set)
+    for line in original_file:
+        # line = re.sub(r'[^0-9]+', ' ', line)
+        line = line.strip()
+        identify, year, month, day, *rev = re.split(r'[\s-]\s*', line)
+        if int(year) != target_year:
+            continue
+        if int(month) != target_month:
+            continue
+        raw_dict[identify].add(day)
+
+    with open(raw_filename, 'wt') as raw_file:
+        for key in raw_dict:
+            for day in raw_dict[key]:
+                txt = key + ' ' +  day + '\n'
+                raw_file.write(txt)
 
 
 def dat2xls(in_filename, out_filename):
@@ -23,8 +42,8 @@ def dat2xls(in_filename, out_filename):
 
     raw_dict = defaultdict(set)
     for line in raw_file:
-        line.strip('\t')
-        identify, month, day, *rev = re.split(r'[\s-]\s*', line)
+        line.strip('')
+        identify,  day, *rev = re.split(r'[\s-]\s*', line)
         raw_dict[identify].add(day)
 
     workbook = xlwt.Workbook(encoding='utf-8')
@@ -45,5 +64,6 @@ def dat2xls(in_filename, out_filename):
 
 
 if __name__ == '__main__':
-    dat2xls('D:/Downloads/4.txt', 'text_xls.xls')
+    analysis_original_file('D:/Downloads/1_attlog.dat', '4.txt', 2017, 4)
+    dat2xls('4.txt', 'text_xls4.xls')
     pass
