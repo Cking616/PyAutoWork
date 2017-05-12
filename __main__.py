36cf8bbe9ee05e0827a6c96aed9170800658bdd4
HaitaoKang <cking616@mail.ustc.edu.cn>
2017-05-12 19:40:03 +0800

    发布稳定版

--- __main__.py
+++ __main__.py
@@ -31,9 +31,13 @@ def generate_style():
     alignment.horz = xlwt.Alignment.HORZ_CENTER  # May be: HORZ_GENERAL, HORZ_LEFT, HORZ_CENTER, HORZ_RIGHT, HORZ_FILLED, HORZ_JUSTIFIED, HORZ_CENTER_ACROSS_SEL, HORZ_DISTRIBUTED
     alignment.vert = xlwt.Alignment.VERT_CENTER  # May be: VERT_TOP, VERT_CENTER, VERT_BOTTOM, VERT_JUSTIFIED, VERT_DISTRIBUTED
 
+    font = xlwt.Font()
+    font.name = 'SimSun'  # 指定“宋体”
+
     style = xlwt.XFStyle()  # Create Style
     style.borders = borders  # Add Borders to Style
     style.alignment = alignment  # Add Alignment to Style
+    style.font = font
     return style
 
 
@@ -76,11 +80,14 @@ def dat2xls(in_filename, out_filename, work_day):
     for i in work_day:
         worksheet.write(2, index, i, style)
         index = index + 1
+    txt = '缺席'
+    worksheet.write(2, index,  txt, style)
 
     index = 3
     while len(raw_dict) != 0:
         key = min(raw_dict.keys())
         worksheet.write(index, 0, key, style)
+        worksheet.write(index, 1, ' ', style)
         y = 2
         for i in work_day:
             if i in raw_dict[key]:
@@ -88,16 +95,32 @@ def dat2xls(in_filename, out_filename, work_day):
             else:
                 worksheet.write(index, y, ' ', style)
             y = y + 1
+        worksheet.write(index, y, ' ', style)
         raw_dict.pop(key)
         index = index + 1
 
+    txt = '工号'
+    worksheet.write_merge(1, 2, 0, 0, txt, style)  # Merges row 0's columns 0 through 3.
+    txt = '姓名'
+    worksheet.write_merge(1, 2, 1, 1,  txt, style)  # Merges row 0's columns 0 through 3.
+    lens = len(work_day)
+    txt = '考勤情况'
+    worksheet.write_merge(1, 1, 2, lens + 2,  txt, style)  # Merges row 1 through 2's columns 0 through 3.
+    txt = '考勤系统录入'
+    worksheet.write_merge(0, 0, 0, lens + 2,  txt, style)  # Merges row 1 through 2's columns 0 through 3.
     if not out_filename.endswith('.xls'):
         out_filename = out_filename + '.xls'
     workbook.save(out_filename)
 
 
 def translate_process():
-    print("用法：第一步，将dat记录文件放到dat文件夹下\n")
+    print("用法：第一步，将dat记录文件放到当前目录下生成的dat文件夹下\n")
+    if not os.path.exists('dat'):
+        os.mkdir('dat')
+    if not os.path.exists('excel'):
+        os.mkdir('excel')
+    if not os.path.exists('txt'):
+        os.mkdir('txt')
     input("完成后请按Enter继续\n")
 
     print("请选择要使用的dat文件，输入之前的数字号\n")
@@ -120,21 +143,25 @@ def translate_process():
         input_choice = int(input("输入的数字不在范围内，请重新输入\n"))
     target_year = input_choice
 
-    input_choice = int(input("请输入筛选的月份(范围1到12)\n"))
-    while input_choice < 1 or input_choice > 12:
-        input_choice = int(input("输入的数字不在范围内，请重新输入\n"))
-    target_month = input_choice
+    print("以下为循环选择，0退出")
+    while input_choice != 0:
+        input_choice = int(input("请输入筛选的月份(范围1到12),输入0退出\n"))
+        if input_choice == 0:
+            break
+        while input_choice < 1 or input_choice > 12:
+            input_choice = int(input("输入的数字不在范围内，请重新输入\n"))
+        target_month = input_choice
 
-    raw_filename = './txt/' + str(target_year) + '-' + str(target_month) + '.txt'
-    xls_filename = './excel/' + str(target_year) + '-' + str(target_month) + '.xls'
+        raw_filename = './txt/' + str(target_year) + '-' + str(target_month) + '.txt'
+        xls_filename = './excel/' + str(target_year) + '-' + str(target_month) + '.xls'
 
-    txt = "在txt文件夹下生成" + raw_filename + '\n'
-    print(txt)
+        txt = "在txt文件夹下生成" + raw_filename + '\n'
+        print(txt)
 
-    txt = "并在excel文件下生成" + raw_filename + '\n'
-    print(txt)
-    work_day = analysis_original_file(original_filename, raw_filename, target_year, target_month)
-    dat2xls(raw_filename, xls_filename, work_day)
+        txt = "并在excel文件下生成" + raw_filename + '\n'
+        print(txt)
+        work_day = analysis_original_file(original_filename, raw_filename, target_year, target_month)
+        dat2xls(raw_filename, xls_filename, work_day)
 
 
 if __name__ == '__main__':
