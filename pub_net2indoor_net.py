1bb8053fdcff4da646c0ae448bce31efa82ba9a6
cking616 <cking616@mail.ustc.edu.cn>
2017-05-15 12:42:42 +0800

    添加公网传剪切板数据到内网的支持支持

--- pub_net2indoor_net.py
+++ pub_net2indoor_net.py
@@ -8,12 +8,34 @@
 @software: PyCharm Community Edition
 @file: pub_net2indoor_net.py
 @time: 2017/5/14 0014 19:11
+@brief：公网至内网传输文件
 """
+import shutil
+import sys
+import os.path
+import pyperclip
 
 
-def func():
-    pass
+def get_clipboard_text():
+    text = pyperclip.paste()
+    return text
+
+
+def set_clipboard_text(text):
+    pyperclip.set_clipboard(text)
+
+
+def clipboard2indoor_net():
+    with open(r'\\172.16.1.111\公网至内网文件\kanghaitao康海涛\pub_net_clipboard.txt', 'wt') as file:
+        text = (get_clipboard_text())
+        file.write(text)
+
+
+def get_pub_net_clipboard():
+    with open(r'\\172.16.10.123\公网至内网文件\kanghaitao康海涛\pub_net_clipboard.txt', 'rt') as file:
+        text = file.read()
+        set_clipboard_text(text)
 
 
 if __name__ == '__main__':
-    pass
+    clipboard2indoor_net()
