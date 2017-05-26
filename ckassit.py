53d644a7cf6834b7cf4b8e8b5d0ee813326d32ac
cking616 <cking616@mail.ustc.edu.cn>
2017-05-27 00:11:06 +0800

    调整获取百度token

--- ckassit.py
+++ ckassit.py
@@ -12,16 +12,17 @@
 import urllib.request
 import wave
 import pycurl
-import base64
 import json
 
 
 def get_token():
-    api_key = ""
-    secret_key = ""
-    auth_url = api_key + secret_key
-    respon = urllib.request.urlopen(auth_url)
-    json_data = respon.read()
+    api_key = "VvYvv9WevLS67TV56onVAIDn"
+    secret_key = "e0628ea2b2823974cfd72d636d5ae740"
+    auth_url = ("https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=" +
+                api_key + "&client_secret=" + secret_key)
+    res = urllib.request.urlopen(auth_url)
+    json_data = res.read()
+    json_data = json_data.decode('utf-8')
     return json.loads(json_data)['access_token']
 
 
@@ -33,7 +34,7 @@ def use_audio_cloud(token, wave_file):
     wave_file_ptr = wave.open(wave_file, 'rb')
     num_of_frames = wave_file_ptr.getnframes()()
     wave_file_len = num_of_frames * 2
-    audio_datas = wave_file_ptr.readframes(num_of_frames)
+    audio_data = wave_file_ptr.readframes(num_of_frames)
 
     cuid = ""
     server_url = 'http://vop.baidu.com/server_api' + '?cuid=' + cuid + '&token=' + token
@@ -50,10 +51,12 @@ def use_audio_cloud(token, wave_file):
     curl.setopt(curl.CONNECTTIMEOUT, 30)
     curl.setopt(curl.TIMEOUT, 30)
     curl.setopt(curl.WRITEFUNCTION, dump_res)
-    curl.setopt(curl.POSTFIELDS, audio_datas)
+    curl.setopt(curl.POSTFIELDS, audio_data)
     curl.setopt(curl.POSTFIELDSIZE, wave_file_len)
     curl.perform()  # pycurlurl.perform() has no return val
 
 
 if __name__ == '__main__':
-    pass
+    token = get_token()
+    print(token)
+
